7bffe2519ac0406d7caf66fecc6a91971e33f47f
YoungCR <PlutoYCR520@outlook.com>
2025-02-10 22:29:51 +0800

    2025.2.10日

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
-import java.lang.reflect.Array;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author YoungCR
@@ -9,29 +9,22 @@ import java.util.*;
 public class Main {
 
 
-    public int maxScore(int[] cardPoints, int k) {
-        int maxScore = 0;
-        int score    = 0;
-
-        // 因为只能从左右两边开始取，那其实可以将这副牌看成一个环状
-        // 从左边一直拿，或者从右边一直拿，分别是两个极限
-        for (int i = cardPoints.length - k; i < cardPoints.length + k; i++) {
-            score += cardPoints[i % cardPoints.length];
-            if (i < cardPoints.length - 1) {
+    public int[] getSubarrayBeauty(int[] nums, int k, int x) {
+        int count = 0;
+        int[] ans = new int[nums.length - k + 1];
+        int
+        for (int i = 0; i < nums.length; i++) {
+            if (i < k - 1) {
                 continue;
             }
-            maxScore = Math.max(maxScore, score);
-            if (i == cardPoints.length + k - 1) {
-                break;
-            }
-            score -= cardPoints[i - k + 1];
+
+
         }
-
-        return maxScore;
     }
-
 
     public static void main(String[] args) {
-        System.out.println(new Main().maxScore(new int[]{96, 90, 41, 82, 39, 74, 64, 50, 30}, 8));
+        int freq = new Main().maxFreq("aababcaab", 2, 3, 4);
+        System.out.println(freq);
     }
+
 }
